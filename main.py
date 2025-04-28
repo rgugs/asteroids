6f9b04eb7b4867e81c494f727686f27397fd40b0
Rgugs <rgugs10@gmail.com>
2025-04-27 23:32:17 -0500

    adding new classes

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     # print(f'Screen height: {SCREEN_HEIGHT}')
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     running = True
 
     while running:
@@ -16,6 +18,7 @@ def main():
     
         screen.fill(color="black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
