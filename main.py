920765d01f347f4c9bb3e96b1ba041356c90b7ef
rgugs <rgugler@gmail.com>
2026-01-21 21:18:24 -0800

    Setup base gameloop

--- main.py
+++ main.py
@@ -9,15 +9,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     running = True
+
     while running:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
